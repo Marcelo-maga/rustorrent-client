efdad7a3f1365c58b6579a656b0dd780e903d741
marcelo <marcelo.maga2550@gmail.com>
2024-10-25 15:44:35 +0000

    feat: add convert for torrent files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use serde_derive::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{self, Read};
+use std::net::UdpSocket;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TorrentFile {
+    announce: String,
+}
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("puppy.torrent")?;
+
+    let mut torrent = Vec::new();
+    file.read_to_end(&mut torrent)?;
+
+    let decoded: TorrentFile =
+        serde_bencode::from_bytes(&torrent).expect("Erro ao decodificar Bencode");
+
+    let torrent_url = decoded.announce;
+
+       prinln!("{}", torrent_url);
+
+    Ok(())
 }
